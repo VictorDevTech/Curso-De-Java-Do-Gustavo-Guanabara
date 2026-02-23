15173b224cc212bd5116b9b64df0ad51faefc3df
Victor Augusto Pereira Da Silva <victor.augusto.devtech@gmail.com>
2026-02-23 17:21:09 -0300

    mudei o nome da variavel

--- Exercicios_De_Java/Idioma_Do_Sistema.java
+++ Exercicios_De_Java/Idioma_Do_Sistema.java
@@ -6,12 +6,12 @@ public class Idioma_Do_Sistema {
 
         // Locale é a classe que guarda informações de idioma e país
         // getDefault() pega a configuração atual do computador
-        Locale loc = Locale.getDefault();
+        Locale localiza_o_idioma_do_sistema = Locale.getDefault();
 
         // loc.getDisplayLanguage()
         // getDisplayLanguage() pega o nome do idioma por extenso
         // Exemplo: Português, English, Español
-        String idioma = loc.getDisplayLanguage();
+        String idioma = localiza_o_idioma_do_sistema.getDisplayLanguage();
 
         // System.out.println()
         // Mostra algo na tela e pula para a próxima linha
