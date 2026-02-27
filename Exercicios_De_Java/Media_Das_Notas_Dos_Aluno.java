16a73518019d40a6fc49d5483f72910acd5724ef
Victor Augusto Pereira Da Silva <victor.augusto.devtech@gmail.com>
2026-02-27 11:55:08 -0300

    Mudei o nome das variaveis

--- Exercicios_De_Java/Media_Das_Notas_Dos_Aluno.java
+++ Exercicios_De_Java/Media_Das_Notas_Dos_Aluno.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 
 public class Media_Das_Notas_Dos_Aluno {
-    
-    public static void main(String[] argd){
 
+    public static void main(String[] args){
 
         Scanner teclado = new Scanner(System.in);
 
-            System.out.print("Digite o nome do aluno: ");
-            String Nome_Aluno = teclado.nextLine();
+        System.out.print("Digite o nome do aluno: ");
+        String nomeAluno = teclado.nextLine();
 
-            System.out.print("Digite a primeira nota: ");
-            float primeira_nota = teclado.nextFloat();
+        System.out.print("Digite a primeira nota: ");
+        float primeiraNota = teclado.nextFloat();
 
-            System.out.print("Digite a segundaa nota: ");
-            float segunda_nota = teclado.nextFloat();
+        System.out.print("Digite a segunda nota: ");
+        float segundaNota = teclado.nextFloat();
 
-            System.out.print("Digite a terceira nota: ");
-            float terceira_nota = teclado.nextFloat();
+        System.out.print("Digite a terceira nota: ");
+        float terceiraNota = teclado.nextFloat();
 
-            System.out.print("Digite a quarta nota: ");
-            float quarta_nota = teclado.nextFloat();
+        System.out.print("Digite a quarta nota: ");
+        float quartaNota = teclado.nextFloat();
 
-            double media_das_notas = (primeira_nota + segunda_nota + terceira_nota + quarta_nota) /4;
+        double media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
 
-            if ( media_das_notas <= 60){
-                System.out.format("O aluno %s foi aprovado e foi com a media %s", Nome_Aluno, media_das_notas);
+        if (media >= 60){
+            System.out.format("O aluno %s foi APROVADO com média %.2f", nomeAluno, media);
+        } else {
+            System.out.format("O aluno %s foi REPROVADO com média %.2f", nomeAluno, media);
+        }
 
-            } else {
-                System.out.format("O aluno %s foi Reprovado e foi com a media %s", Nome_Aluno,media_das_notas);
-            }
+        teclado.close();
     }
 }
-
